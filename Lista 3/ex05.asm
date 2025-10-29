d4059401eb787b0315f9681a229062017daf32d8
Raquel <raquelmartiniano14@gmail.com>
2025-10-29 14:58:34 -0300

    Corrigido, mas não testado

--- Lista 3/ex05.asm
+++ Lista 3/ex05.asm
@@ -1,6 +1,7 @@
 .text
 main: addi $9, $0, 0 #i = 0
       addi $10, $0, 10 # 10 quantidade de vezes
+      addi $21, $0, 0 #acumulador definido antes da soma
 
 soma: beq $9, $10, fimlaco
 
@@ -16,8 +17,8 @@ soma: beq $9, $10, fimlaco
       add $22, $0, $2
       syscall
 
-      add $23, $20, $21 #soma de a e b que dá c
-      add $21, $21, $20 #acumulação, de x = x + c, danod o total da soma
+      add $23, $20, $22 #soma de a e b que dá c
+      add $21, $21, $23 #acumulação, de x = x + c, danod o total da soma
       addi $9, $9, 1 #i + 1
 
       j soma
