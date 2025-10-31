6a18a008fa9e916b5e2731e6dfded3c3fa2ff4e6
Raquel <raquelmartiniano14@gmail.com>
2025-10-30 23:49:31 -0300

    feita
    Refactor assembly code for better readability and structure.

--- Lista 3/ex15.asm
+++ Lista 3/ex15.asm
@@ -1,47 +1,48 @@
 .text
 main:
-	add $2, $0, 5
-	syscall
-	
-	add $20, $0, $2 #n de linhas
-	addi $21, $0, 1 #i == 1
-	addi $22, $0, 1 #contador = 1
-	
-lacoexterno: 	
-	beq $21, $20, fimlaco
-	
-	addi $4, $0, '\n'
-	addi $2, $0, 11
-	syscall
-	
-	addi $20, $20, 1 #soma + 1
+    add $2, $0, 5       # n de linhas
+    syscall
 
-	addi $22, $0, 1 #j = 1, já defino ele antes de iniciar o outro laço, como faço os outros
-	
-lacointerno:  
-	beq $22, $21, fiminterno #se j == i, irá finalizar o dentro laço para externo
-	
-	add $4, $0, $20
-	addi $2, $0, 1
-	syscall
+    add $20, $0, $2    
+    addi $21, $0, 1     # i = 1
+    addi $23, $0, 1     # somador
 
-	addi $4, $0, ' ' #espaço para aumentar a coluna
-	addi $2, $0, 11
-	syscall
+lacoexterno:
+    addi $24, $21, 0   
+    addi $25, $20, 1    # $25 = n + 1
+    beq $24, $25, fimlaco  # se i == n +1, termina
 
-	addi $20, $20, 1 #soma + 1
+    # imprime primeira coluna
+    add $4, $0, $23
+    add $2, $0, 1
+    syscall
 
-	addi $22, $22, 1 #j + 1
+    addi $23, $23, 1      # somador + 1
+    addi $22, $0, 1       # j = 1, contador interno
 
-	j lacointerno
+lacointerno:
+    beq $22, $21, fiminterno  # se j == i, termina interno
 
-fiminterno: 
-		
-	addi $21, $21, 1 #i + 1
+    addi $4, $0, ' '          # espaço
+    addi $2, $0, 11
+    syscall
 
-	j lacoexterno
-	
-fimlaco: addi $2, $0, 10
-	syscall
+    add $4, $0, $23           # imprime número
+    addi $2, $0, 1
+    syscall
 
+    addi $23, $23, 1          # contador +1
+    addi $22, $22, 1          # j +1
+    j lacointerno
 
+fiminterno:
+    addi $4, $0, '\n'   
+    addi $2, $0, 11
+    syscall
+
+    addi $21, $21, 1          # i + 1
+    j lacoexterno             # volta para externo
+
+fimlaco:
+    addi $2, $0, 10
+    syscall
